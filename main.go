d20b6bae4be183a6c363693bc1afa08d9d219e4a
Rogee <rogeecn@qq.com>
2024-02-23 17:58:04 +0800

    add debug

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ import (
 	"github.com/spf13/viper"
 )
 
-var cfgFile string
+var (
+	cfgFile string
+	debug   bool
+)
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -28,8 +31,9 @@ func main() {
 }
 
 func init() {
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.btrenamer.yaml)")
+	rootCmd.PersistentFlags().StringVarP(&cfgFile, "config", "c", "", "config file (default is $HOME/.btrenamer.yaml)")
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolVarP(&debug, "debug", "d", false, "is debug mode")
 }
 
 func run(cmd *cobra.Command, args []string) error {
@@ -58,12 +62,15 @@ func run(cmd *cobra.Command, args []string) error {
 
 	log.Println("rules:", rule.Rules)
 	log.Println("match: ", args)
-	args = []string{
-		"./test/【高清影视之家发布 www.HDBTHD.com】飞鸭向前冲[高码版][国英多音轨+中文字幕].Migration.2023.2160p.HQ.WEB-DL.H265.DDP5.1.2Audio-DreamHD",
-		"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第04-05集][国语音轨+简繁英字幕].The.Hunter.S01.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
-		"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第01-03集][国语音轨+简繁英字幕].The.Hunter.S01.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
-		"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第04-05集][国语音轨+简繁英字幕].The.Hunter.S02.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
-		"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第01-03集][国语音轨+简繁英字幕].The.Hunter.S02.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
+
+	if debug {
+		args = []string{
+			"./test/【高清影视之家发布 www.HDBTHD.com】飞鸭向前冲[高码版][国英多音轨+中文字幕].Migration.2023.2160p.HQ.WEB-DL.H265.DDP5.1.2Audio-DreamHD",
+			"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第04-05集][国语音轨+简繁英字幕].The.Hunter.S01.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
+			"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第01-03集][国语音轨+简繁英字幕].The.Hunter.S01.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
+			"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第04-05集][国语音轨+简繁英字幕].The.Hunter.S02.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
+			"./test/【高清剧集网发布 www.DDHDTV.com】猎冰[第01-03集][国语音轨+简繁英字幕].The.Hunter.S02.2024.1080p.WeTV.WEB-DL.H264.AAC-BlackTV",
+		}
 	}
 
 	for _, rawPath := range args {
