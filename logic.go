c96e604d1a5d34fdf4a6969c4a07c2319e342214
Rogee <rogeecn@qq.com>
2024-02-26 16:02:24 +0800

    fix move cross device issues

--- logic.go
+++ logic.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"io/fs"
 	"os"
 	"path"
@@ -80,7 +81,7 @@ func moveFiles(from, to string, rule Config, ruleIdx int) error {
 			continue
 		}
 
-		if err := os.Rename(filepath.Join(from, file.Name()), filepath.Join(newPath, file.Name())); err != nil {
+		if err := move(filepath.Join(from, file.Name()), filepath.Join(newPath, file.Name())); err != nil {
 			return err
 		}
 	}
@@ -95,4 +96,42 @@ func isFileToSmall(f fs.DirEntry) (bool, error) {
 	}
 
 	return fi.Size() < 1024*1024, nil
+}
+
+func move(source, destination string) error {
+	err := os.Rename(source, destination)
+	if err != nil && strings.Contains(err.Error(), "invalid cross-device link") {
+		return moveCrossDevice(source, destination)
+	}
+	return err
+}
+
+func moveCrossDevice(source, destination string) error {
+	src, err := os.Open(source)
+	if err != nil {
+		return errors.Wrap(err, "Open(source)")
+	}
+	dst, err := os.Create(destination)
+	if err != nil {
+		src.Close()
+		return errors.Wrap(err, "Create(destination)")
+	}
+	_, err = io.Copy(dst, src)
+	src.Close()
+	dst.Close()
+	if err != nil {
+		return errors.Wrap(err, "Copy")
+	}
+	fi, err := os.Stat(source)
+	if err != nil {
+		os.Remove(destination)
+		return errors.Wrap(err, "Stat")
+	}
+	err = os.Chmod(destination, fi.Mode())
+	if err != nil {
+		os.Remove(destination)
+		return errors.Wrap(err, "Stat")
+	}
+	os.Remove(source)
+	return nil
 }
